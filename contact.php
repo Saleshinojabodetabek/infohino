e5da2265335ab0919624200af7fcbdb25740548a
infohino29-ux <info.hino29@gmail.com>
2025-12-17 10:28:47 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -41,6 +41,22 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <!DOCTYPE html>
 <html lang="en">
   <head>
+
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17798394558"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-17798394558');
+</script>
+
+<!-- Event snippet for Website lead conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-17798394558/8czxCKm3zNIbEL7l96ZC'});
+</script>
+
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Hubungi Kami | Nisa Hino </title>
